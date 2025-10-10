d7d6a24b1d622074d489d69112925be5f31ae3b3
Vasya <VasyaPain2004@gmail.com>
2025-10-10 16:21:49 +0900

    fix first task

--- hello_world2.c
+++ hello_world2.c
@@ -8,18 +8,19 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    int flag = 0;
+    int message = 0;
 
     if (world_rank == 0) {
-        printf("Hello World from rank %d out of %d\n", world_rank, world_size);
+        printf("Hello World from rank %d out of %d message %d \n", world_rank, world_size, message);
         if (world_size > 1) {
-            MPI_Send(&flag, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            MPI_Send(&message, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
         }
     } else {
-        MPI_Recv(&flag, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Hello World from rank %d out of %d\n", world_rank, world_size);
+        MPI_Recv(&message, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        message = world_rank;
+        printf("Hello World from rank %d out of %d message %d \n", world_rank, world_size, message);
         if (world_rank < world_size - 1) {
-            MPI_Send(&flag, 1, MPI_INT, world_rank + 1, 0, MPI_COMM_WORLD);
+            MPI_Send(&message, 1, MPI_INT, world_rank + 1, 0, MPI_COMM_WORLD);
         }
     }
 
